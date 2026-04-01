c4dc68f317cb44d51c5ca3dbe6c2793d97fed885
AYFG <sek82468246@gmail.com>
2026-04-01 22:41:50 +0900

    static, MVC 템플릿 엔진

--- hello-spring/src/main/java/hello/hello_spring/controller/helloController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/helloController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class helloController {
@@ -14,4 +15,16 @@ public class helloController {
         model.addAttribute("data","spring!!");
         return "hello";
     }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name")String name, Model model){
+        model.addAttribute("name",name);
+        //http://localhost:8080/hello-mvc?name=spring
+        // 쿼리스트링 name=spring → @RequestParam이 꺼냄
+        return "hello-template";
+    }
 }
+/*
+* ./gradlew build
+* cd build/libs
+* java -jar hello-spring-0.0.1-SNAPSHOT.jar
+* */
