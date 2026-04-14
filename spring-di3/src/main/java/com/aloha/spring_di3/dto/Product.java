7a5d7e49997c1935a91717a208c2236b688c6297
AYFG <sek82468246@gmail.com>
2026-04-14 22:47:24 +0900

    @Configuration, @Bean 생성자 주입 권장 방법까지

--- spring-di3/src/main/java/com/aloha/spring_di3/dto/Product.java
+++ spring-di3/src/main/java/com/aloha/spring_di3/dto/Product.java
@@ -1,5 +1,14 @@
 package com.aloha.spring_di3.dto;
 
-public class Product {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private String productName;
+    private int price;
+    private int stock;
 }
--- spring-di3/src/main/java/com/aloha/spring_di3/dto/Users.java
+++ spring-di3/src/main/java/com/aloha/spring_di3/dto/Users.java
@@ -5,8 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor // 파라미터 없는 기본 생성자 자동 생성
+@AllArgsConstructor // 모든 필드를 받는 생성자 자동 생성
 public class Users {
     private String username;
     private String password;
