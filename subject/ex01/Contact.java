5e9e093b585cf36c33be408f57264dd4cc0133fb
AYFG <sek82468246@gmail.com>
2026-04-28 22:41:30 +0900

    scanner 진행중

---
+++ subject/ex01/Contact.java
@@ -0,0 +1,8 @@
+//이름, 성, 별명, 전화번호, 비밀 
+public class Contact {
+    String firstName;
+    String lastName;
+    String nickName;
+    String phoneNumber;
+    String secret;
+}
---
+++ subject/ex01/PhoneBook.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PhoneBook {
+    public static void main(String[] args) {
+        Contact[] contact = new Contact[8];
+
+        Scanner scanner = new Scanner(System.in);
+
+        if (scanner.nextLine() == "EXIT") {
+            return;
+        }
+    }
+}
