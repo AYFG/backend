32ac3031eac619e3bb8e29087356c43fa7188302
AYFG <sek82468246@gmail.com>
2026-04-29 17:28:22 +0900

    scanner

--- subject/ex01/PhoneBook.java
+++ subject/ex01/PhoneBook.java
@@ -1,15 +1,30 @@
 import java.util.Scanner;
 
 public class PhoneBook {
+    public static void input(){
+        
+    }
     public static void main(String[] args) {
         Contact[] contact = new Contact[8];
-
-        Scanner scanner = new Scanner(System.in);
-	String str= scanner.next();
-        if (str.equals("EXIT")) {
-		System.out.println("eexxiitt");
-        }else{
-		System.out.println(str);
-	}
+        
+        
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+	        String str= scanner.next();
+            switch (str) {
+                case "ADD":
+                    System.out.println("ADD");
+                    break;
+                case "SEARCH":
+                    System.out.println("SEARCH");
+                    break;
+                case "EXIT":
+                    System.out.println("EXIT");
+                    break;
+                default:
+                    System.out.println("Not Found");
+                    break;
+            }
+        }
     }
 }
