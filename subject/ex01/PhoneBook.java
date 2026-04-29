e56ffc21a938cdf2ca38701ba06d2226c8d9e546
AYFG <sek3137@naver.com>
2026-04-30 08:24:29 +0900

    01 진행

--- subject/ex01/PhoneBook.java
+++ subject/ex01/PhoneBook.java
@@ -3,28 +3,35 @@ import java.util.Scanner;
 public class PhoneBook {
     public static void main(String[] args) {
         Contact[] contact = new Contact[8];
+        Scanner scanner = new Scanner(System.in);
         
         while(true){
-            Scanner scanner = new Scanner(System.in);
-	        String str= scanner.next();
+            if (!scanner.hasNext()) {
+                break;
+            }
+            String str = scanner.next();
             input(str);
+            if (str.equals("EXIT")) {
+                break;
+            }
         }
-
-	public static void input(String str){
-        switch (str) {
-                case "ADD":
-                    System.out.println("ADD");
-                    break;
-                case "SEARCH":
-                    System.out.println("SEARCH");
-                    break;
-                case "EXIT":
-                    System.out.println("EXIT");
-                    break;
-                default:
-                    System.out.println("Not Found");
-                    break;
-
+        scanner.close();
     }
+
+    public static void input(String str){
+        switch (str) {
+            case "ADD":
+                System.out.println("ADD");
+                break;
+            case "SEARCH":
+                System.out.println("SEARCH");
+                break;
+            case "EXIT":
+                System.out.println("EXIT");
+                break;
+            default:
+                System.out.println("Not Found");
+                break;
+        }
     }
 }
